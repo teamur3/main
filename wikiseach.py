56de0e66f86a9d9667c3a5ed08945e434815d521
teamur3 <102431239+teamur3@users.noreply.github.com>
2022-03-28 11:29:46 +0500

    Add files via upload

---
+++ wikiseach.py
@@ -0,0 +1,30 @@
+from wikipedia import *
+
+i = input('Choose language>')
+set_lang(i)
+
+
+cycle = True
+
+print ('WikiSeach by teamur\nVer 0.0.2')
+
+while cycle == True:
+    i = input('seach>')
+    
+    if i == "/exit":
+        cycle = False
+        
+    else:
+        page = wikipedia.page(i)
+        print (page.title)
+        print ('Url -', page.url)
+        con = page.content
+        print(page.content)
+        
+        c = input('Save article?(y/n)')
+        if c == 'y':
+            path = input('Enter path to file>')
+            file = open(path, "w")
+            file.write(con)
+            file.close
+        else: 0
